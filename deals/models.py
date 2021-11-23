5d7e7acb00e7b4d3a528d16214b004a01d02cf15
KoteckiS <sergiusz.kotecki@trw.com>
2021-11-23 18:49:59 +0100

    Deals model refactor

--- deals/models.py
+++ deals/models.py
@@ -15,12 +15,20 @@ class Deal(models.Model):
         default=uuid.uuid4,
         editable=False)
     current_price = models.DecimalField(max_digits=6, decimal_places=2)
-    historical_price = models.DecimalField(max_digits=6, decimal_places=2)
+    historical_price = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=True, default=0)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     valid_till = models.DateTimeField(null=True, blank=False)
-    pub_date = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def price_percentage(self):
+        test = self.current_price / self.historical_price * 100 if self.historical_price > 0 else 0
+        if test:
+            print(type(test))
+            return test
+        else:
+            return 0
 
     def __str__(self):
         return self.name
@@ -30,6 +38,8 @@ class Comment(models.Model):
     author: CustomUser = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     deal = models.ForeignKey(Deal, on_delete=models.CASCADE)
     comment = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.author.first_name if self.author.first_name else self.author.username
