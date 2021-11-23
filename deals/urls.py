ee733d4691c64da7784b17cb3b07fe5138cae0a5
KoteckiS <sergiusz.kotecki@trw.com>
2021-11-23 18:50:44 +0100

    Add deal details to URLs

--- deals/urls.py
+++ deals/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from deals.views import DealDetailView
 from deals.views import DealListView
 
 urlpatterns = [
-    path('', DealListView.as_view(), name='list')
+    path('', DealListView.as_view(), name='list'),
+    path('deal-detail/<int:pk>/', DealDetailView.as_view(), name='detail')
 ]
