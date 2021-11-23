4009452844f54e9760d1ea760e24678697e01dba
KoteckiS <sergiusz.kotecki@trw.com>
2021-11-23 18:51:09 +0100

    Prepared Views for deal details

--- deals/views.py
+++ deals/views.py
@@ -1,11 +1,24 @@
 # Create your views here.
+from django.views.generic import DetailView
 from django.views.generic import ListView
 
+from deals.models import Comment
 from deals.models import Deal
 
 
 class DealListView(ListView):
     model = Deal
-    template_name = 'deal/deal_list.html'
+    template_name = 'deals/deal_list.html'
     context_object_name = 'deals'
+
+
+class DealDetailView(DetailView):
+    model = Deal
+    template_name = 'deals/deal_detail.html'
+    context_object_name = 'deal'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = Comment.objects.filter(deal=self.object.id).order_by('-created_at')
+        return context
 
