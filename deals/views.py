92cd46f9dffbb80d0f3fd3e7243f039c43ff0cb1
KoteckiS <sergiusz.kotecki@trw.com>
2021-11-24 08:24:50 +0100

    Add view for product creation

--- deals/views.py
+++ deals/views.py
@@ -1,4 +1,5 @@
 # Create your views here.
+from django.views.generic import CreateView
 from django.views.generic import DetailView
 from django.views.generic import ListView
 
@@ -12,13 +13,27 @@ class DealListView(ListView):
     context_object_name = 'deals'
 
 
+class DealCreateView(CreateView):
+    model = Deal
+    template_name = 'deals/deal_create.html'
+    fields = [
+        'name',
+        'description', 'link',
+        'current_price', 'historical_price', 'delivery_cost',
+        'valid_till'
+    ]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
 class DealDetailView(DetailView):
     model = Deal
     template_name = 'deals/deal_detail.html'
     context_object_name = 'deal'
 
     def get_context_data(self, **kwargs):
-        from django.core.paginator import Paginator
         context = super().get_context_data(**kwargs)
         comments = Comment.objects.filter(deal=self.object.id).order_by('-created_at')[:10]
         context['comments'] = comments
