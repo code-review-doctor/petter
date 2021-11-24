12b5472767cef9bb5d908f3d6bbd9bfc6bdf2842
KoteckiS <sergiusz.kotecki@trw.com>
2021-11-24 08:22:56 +0100

    Add URL for new product addition, shortened url for deal detail

--- deals/urls.py
+++ deals/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from deals.views import DealCreateView
 from deals.views import DealDetailView
 from deals.views import DealListView
 
 urlpatterns = [
     path('', DealListView.as_view(), name='list'),
-    path('deal-detail/<int:pk>/', DealDetailView.as_view(), name='detail')
+    path('deal/<int:pk>/', DealDetailView.as_view(), name='detail'),
+    path('deal/new/', DealCreateView.as_view(), name='new'),
 ]
