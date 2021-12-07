d5463f2b8df70bef275445f38f9c35353e92f867
KoteckiS <sergiusz.kotecki@trw.com>
2021-12-07 12:59:21 +0100

    Configuration for coverage

--- manage.py
+++ manage.py
@@ -7,6 +7,20 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+    try:
+        command = sys.argv[1]
+    except IndexError:
+        command = 'help'
+
+    running_test = command == 'test'
+
+    if running_test:
+        from coverage import Coverage
+        coverage = Coverage()
+        coverage.erase()
+        coverage.start()
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,6 +30,15 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+    if running_test:
+        coverage.stop()
+        coverage.save()
+        covered = coverage.report()
+        coverage.xml_report()
+        coverage.erase()
+        if covered < 80:
+            sys.exit(1)
 
 
 if __name__ == '__main__':
